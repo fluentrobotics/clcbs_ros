f764c5c4ea4118478aadb3d9e368742de2638acb
Jeeho Ahn <jeeho@umich.edu>
2024-03-11 19:07:47 -0400

    Update cars.yaml, init_clcbs.launch, and init_clcbs.py

--- src/init_clcbs.py
+++ src/init_clcbs.py
@@ -10,13 +10,65 @@ from copy import deepcopy
 
 testing_standalone = False  # set to false if testing the whole system as one unit. When testing as standalone, don't launch clcbs_node
 
-use_mocap_start = False  # set to true to use the mocap position as the start position, false to use the testcase start position
+use_mocap_start = True  # set to true to use the mocap position as the start position, false to use the testcase start position
 
 INVALID_POSE_Z = -100.0
 mocap_poses = []
 
-def mocap_callback(p, i):
+#temporary use to add coord offset
+x_off_mocap = 2.25
+y_off_mocap = 2.85
+
+def rotate_quaternion(original_quaternion, angle=180):
+    import tf.transformations as tftf
+    # Convert the angle to radians
+    angle_rad = angle * (3.14159 / 180.0)
+
+    # Create a quaternion representing the rotation around the z-axis
+    rotation_quaternion = tftf.quaternion_about_axis(angle_rad, (0, 0, 1))
+
+    # Convert the original quaternion to a list
+    original_quaternion_list = [original_quaternion.x, original_quaternion.y, original_quaternion.z, original_quaternion.w]
+
+    # Multiply the original quaternion by the rotation quaternion
+    rotated_quaternion_list = tftf.quaternion_multiply(original_quaternion_list, rotation_quaternion)
+
+    # Create a new Quaternion message with the rotated quaternion
+    rotated_quaternion_msg = Quaternion()
+    rotated_quaternion_msg.x = rotated_quaternion_list[0]
+    rotated_quaternion_msg.y = rotated_quaternion_list[1]
+    rotated_quaternion_msg.z = rotated_quaternion_list[2]
+    rotated_quaternion_msg.w = rotated_quaternion_list[3]
+
+    return rotated_quaternion_msg
+
+
+
+def mocap_callback(p:PoseStamped, i):
     mocap_poses[i] = p
+    #todo: make it a transformation matrix
+    #apply offset on mocap coord
+    #p is not being copied by = operator
+    #p_x = p.pose.position.x
+    #p_y = p.pose.position.y
+    #p_off_mocap = PoseStamped()
+    #p_off_mocap.header = p.header
+    #p_off_mocap.pose.position.x = p_x - x_off_mocap
+    #p_off_mocap.pose.position.y = p_y - y_off_mocap
+    #print(p_off_mocap)
+    #change of base
+    #p_off_ros = PoseStamped()
+    #p_off_ros.header = p.header
+    #p_off_ros.pose.position.x = -1.0*p_off_mocap.pose.position.x
+    #p_off_ros.pose.position.y = -1.0*p_off_mocap.pose.position.y
+    #print("hi")
+
+    #p_off_ros.pose.orientation = rotate_quaternion(p.pose.orientation)
+
+
+    #print(p_off_ros)
+    #mocap_poses[i] = p_off_ros
+
 
 def angle_to_quaternion(angle):
     """Convert an angle in radians into a quaternion _message_."""
@@ -56,7 +108,9 @@ if __name__ == "__main__":
         if use_mocap_start:
             mocap_poses.append(PoseStamped())
             mocap_poses[i].pose.position.z = INVALID_POSE_Z
-            mocap_sub = rospy.Subscriber("/" + name + "/car_pose", PoseStamped, callback=mocap_callback, callback_args=i)
+            #mocap_sub = rospy.Subscriber("/" + name + "/car_pose", PoseStamped, callback=mocap_callback, callback_args=i)
+            mocap_sub = rospy.Subscriber("/natnet_ros/" + name + "/pose_tfed", PoseStamped, callback=mocap_callback, callback_args=i)
+            print("name: " + "/natnet_ros/" + name + "/pose_tfed")
             mocap_subs.append(mocap_sub)
 
     goal_pub = rospy.Publisher(
@@ -76,7 +130,7 @@ if __name__ == "__main__":
             carmsg.pose = deepcopy(mocap_poses[i].pose)
             carmsg.pose.position.z = 0.0
         else:
-            start_pose = rospy.get_param("/init_clcbs/car" + str(i + 1) + "/start")
+            start_pose = rospy.get_param("/init_clcbs/mushr" + str(i + 1) + "/start") #todo: name
             carmsg.pose.position.x = min(x_max, max(x_min, start_pose[0] + random.uniform(-randomness[0], randomness[0])))
             carmsg.pose.position.y = min(y_max, max(y_min, start_pose[1] + random.uniform(-randomness[1], randomness[1])))
             carmsg.pose.position.z = 0.0
@@ -109,7 +163,7 @@ if __name__ == "__main__":
     goalmsg.maxy = y_max
     for i in range(num_agent):
         goalmsg.goals.append(PoseArray())
-        waypoints = rospy.get_param("/init_clcbs/car" + str(i + 1) + "/waypoints")
+        waypoints = rospy.get_param("/init_clcbs/mushr" + str(i + 1) + "/waypoints") #todo: name
         for j in range(num_waypoint):
             goal = Pose()
             goal.position.x = waypoints[j][0]
